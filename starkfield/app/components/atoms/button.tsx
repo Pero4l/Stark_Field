248247bc7b17aef4588a41670292af543f01a0f0
pero4l <lilpero03@gmail.com>
2025-12-13 18:29:02 +0100

    lateset commit

--- starkfield/app/components/atoms/button.tsx
+++ starkfield/app/components/atoms/button.tsx
@@ -15,19 +15,19 @@ const Button = ({ onClose }: ButtonProps) => {
 
       {/* BUTTONS */}
       <div className="flex  justify-center left-5 lg:flex relative lg:left-10">
-        <Link
+        {/* <Link
           href="/auth/register"
           className="bg-green-600 text-white lg:px-12 lg:w-fit md:px-32 px-12 py-3 rounded-l-full hover:bg-green-700 transition"
           onClick={onClose}
         >
           Sign Up
-        </Link>
+        </Link> */}
         <Link
           href="/auth/login"
-          className="border border-green-600 text-green-600 lg:px-12 lg:w-fit md:px-32 px-10 py-3 rounded-full hover:bg-green-600 hover:text-white transition bg-white relative md:right-4 lg:right-7 right-7"
+          className="border border-green-600 text-green-600 lg:px-12 lg:w-fit md:px-32 px-10 py-3 font-bold rounded-full hover:bg-green-600 hover:text-white transition bg-white relative md:right-4 lg:right-7 right-7"
           onClick={onClose}
         >
-          Sign In
+          Connect Wallet
         </Link>
       </div>
     </div>
